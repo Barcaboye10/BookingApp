6d350013b2605ff5d67206c521a1bad7969ca56d
Barcaboye10 <rajgupta2140@gmail.com>
2022-07-14 14:00:23 +0530

    User Sign up and login

---
+++ api/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match:
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    // country: {
+    //   type: String,
+    //   required: true,
+    // },
+    // img: {
+    //   type: String,
+    // },
+    // city: {
+    //   type: String,
+    //   required: true,
+    // },
+    // phone: {
+    //   type: String,
+    //   required: true,
+    // },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+//   If you set timestamps: true , Mongoose will add two properties of type Date to your schema: createdAt and updatedAt
+);
+ export default mongoose.model("User", userSchema);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
+import { loginUser, registerUser } from "../controllers/auth.js";
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.send("Hello from Auth")
-})
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
